447b08ed156d30d4e8ed43d18750bf2cd088caa0
saviraputri21 <saviraputripramudita20@gmail.com>
2025-07-18 16:06:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -52,20 +52,18 @@ elif menu == "📊 Dashboard":
     **1. Hukum Boyle**  
     Pada suhu tetap, volume berbanding terbalik dengan tekanan.  
     PV = Konstan
-    
     P1.V1 = P2.V2
 
     **2. Hukum Charles**  
     Pada tekanan tetap, volume berbanding lurus dengan suhu.  
-    \[
-    V \propto T
-    \]
+    VT = Konstan 
+    V1/T1 = V2/T2
+  
 
     **3. Hukum Gay-Lussac**  
     Pada volume tetap, tekanan berbanding lurus dengan suhu.  
-    \[
-    P \propto T
-    \]
+    P/T = Konstan
+    P1/T1 = P2/T2
 
     ---
 
@@ -113,12 +111,12 @@ elif menu == "👥 Tentang Kami":
    
    📘Disusun oleh Kelompok 2
    
-     Anggota Kelompok: 
-     -Dhelis
-     -Ismil Aziz
-     -Mutia Ningrum
-     -Savira Putri Pramudita
-     -Azka
+   Anggota Kelompok: 
+   - Dhelis
+   -Ismil Aziz
+   -Mutia Ningrum
+   -Savira Putri Pramudita
+   -Azka
     """)
 
 # ================================
