7b81e42326cf72c4d4e8bc59b651d760a1e5a006
saviraputri21 <saviraputripramudita20@gmail.com>
2025-07-25 06:16:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-color: #e0f7fa;
+        background-color: #004D40;
         background-image: url('https://cdn.pixabay.com/photo/2017/08/30/07/52/chemistry-2696850_1280.png'); /* Struktur atom/kimia */
         background-size: contain;
         background-repeat: no-repeat;
